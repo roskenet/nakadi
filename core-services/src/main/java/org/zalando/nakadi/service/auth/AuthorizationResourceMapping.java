c2576bb12aa6e4ad15478adc37db55bf6b109b1f
Aleksey Pak <aleksey.pak@zalando.de>
2024-05-17 18:04:01 +0200

    aruha/team-aruha#1740 Pass additional attributes of event type to the auth policy rules

--- core-services/src/main/java/org/zalando/nakadi/service/auth/AuthorizationResourceMapping.java
+++ core-services/src/main/java/org/zalando/nakadi/service/auth/AuthorizationResourceMapping.java
@@ -5,6 +5,8 @@ import org.zalando.nakadi.domain.ResourceImpl;
 import org.zalando.nakadi.plugin.api.authz.EventTypeAuthz;
 import org.zalando.nakadi.plugin.api.authz.Resource;
 
+import javax.annotation.Nullable;
+
 public class AuthorizationResourceMapping {
     public static final String DATA_COMPLIANCE_ASPD_CLASSIFICATION_ANNOTATION =
             "compliance.zalando.org/aspd-classification";
@@ -13,7 +15,18 @@ public class AuthorizationResourceMapping {
         return new ResourceImpl<>(
                 eventType.getName(),
                 ResourceImpl.EVENT_TYPE_RESOURCE,
-                eventType.getAuthorization(),
+                new EventTypeAuthorization(
+                        eventType.getAuthorization(),
+                        getAspdDataClassification(eventType),
+                        eventType.getEventOwnerSelector()),
                 eventType);
+    }
+
+    private static @Nullable String getAspdDataClassification(final EventTypeBase eventType) {
+        final var annotations = eventType.getAnnotations();
+        if (annotations != null) {
+            return annotations.get(DATA_COMPLIANCE_ASPD_CLASSIFICATION_ANNOTATION);
+        }
+        return null;
     }
 }
