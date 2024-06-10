e33d45f83fe4d233c5e67cc89aa4924d1cdc48af
ssaha <samrat.saha@zalando.de>
2024-06-10 13:08:47 +0200

    remove null check, so downstream we can do logic on it

--- core-services/src/main/java/org/zalando/nakadi/service/auth/EventTypeAuthorization.java
+++ core-services/src/main/java/org/zalando/nakadi/service/auth/EventTypeAuthorization.java
@@ -43,7 +43,7 @@ class EventTypeAuthorization implements ValidatableAuthorization {
             final AuthorizationService.Operation operation) throws IllegalArgumentException {
         final Optional<List<AuthorizationAttribute>> attributes = authorization != null ?
                 authorization.getAttributesForOperation(operation) : Optional.empty();
-        if (operation == AuthorizationService.Operation.READ && aspdDataClassification != null) {
+        if (operation == AuthorizationService.Operation.READ) {
             final var extendedAttributes = new ArrayList<AuthorizationAttribute>();
             attributes.ifPresent(extendedAttributes::addAll);
             // TODO: this is coupling in the core code with Auth plugin logic.
