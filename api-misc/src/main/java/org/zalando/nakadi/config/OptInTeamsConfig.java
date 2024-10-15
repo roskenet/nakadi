79a33be4d2a86826a63d77d7c91015c8af171d8b
Chaitanya Waikar <chaitanya.waikar@zalando.de>
2024-10-15 12:42:52 +0200

    Add helper class to read opt in teams config periodically (#168)
    * Add helper class to read opt in teams config periodically
    
    * Incorporate review comments
    
    * Update api-misc/src/main/java/org/zalando/nakadi/config/OptInTeamsConfig.java
    
    Co-authored-by: Aleksey Pak <aleksey.pak@zalando.de>
    
    * Update api-misc/src/main/java/org/zalando/nakadi/config/OptInTeamsConfig.java
    
    Co-authored-by: Aleksey Pak <aleksey.pak@zalando.de>
    
    * Fix compilation issue.
    
    * Incorporate review comments.
    
    ---------
    
    Co-authored-by: Aleksey Pak <aleksey.pak@zalando.de>

---
+++ api-misc/src/main/java/org/zalando/nakadi/config/OptInTeamsConfig.java
@@ -0,0 +1,77 @@
+package org.zalando.nakadi.config;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+@Component
+@EnableScheduling
+public class OptInTeamsConfig {
+
+    private final Resource resource;
+    private final ObjectMapper objectMapper;
+    private volatile Set<String> optInTeams;
+
+    private static final Logger LOG = LoggerFactory.getLogger(OptInTeamsConfig.class);
+
+    static class TeamIds {
+        @JsonProperty("team-ids")
+        private List<String> teamIds;
+
+        public List<String> getTeamIds() {
+            return teamIds;
+        }
+
+        public void setTeamIds(final List<String> teamIds) {
+            this.teamIds = teamIds;
+        }
+    }
+
+    @Autowired
+    public OptInTeamsConfig(
+            @Value("${nakadi.aspd.opt-in.teams:aspd-opt-in-teams.json}") final Resource configuration,
+            final ObjectMapper objectMapper
+    ) {
+        this.resource = configuration;
+        this.optInTeams = Collections.emptySet();
+        this.objectMapper = objectMapper;
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        reloadConfigurationIfChanged();
+    }
+
+    @Scheduled(fixedDelay = 10_000)
+    public void checkConfigurationChange() {
+        LOG.trace("Checking if configuration is changed");
+        reloadConfigurationIfChanged();
+    }
+
+    public Set<String> getOptInTeams() {
+        return optInTeams;
+    }
+
+    private void reloadConfigurationIfChanged() {
+        try (InputStream in = resource.getInputStream()) {
+            final TeamIds teamIds = objectMapper.readValue(in, TeamIds.class);
+            optInTeams = Set.copyOf(teamIds.getTeamIds());
+        } catch (IOException ex) {
+            LOG.warn("Failed to read opt-in configuration from resource {}", resource, ex);
+        }
+    }
+}
