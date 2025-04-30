48e45681d472c1b498077e9dabd27abb5123cf44
Filippo Ghibellini <filippo.ghibellini@zalando.de>
2025-04-30 17:10:17 +0200

    Small changes

--- api-misc/src/main/java/org/zalando/nakadi/controller/DebugController.java
+++ api-misc/src/main/java/org/zalando/nakadi/controller/DebugController.java
@@ -1,22 +1,25 @@
 package org.zalando.nakadi.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.zalando.aruha.nakadisql.api.criteria.Criterion;
 import org.zalando.aruha.nakadisql.parser.nsql.SqlParserException;
-import org.zalando.nakadi.exceptions.runtime.*;
-import org.zalando.nakadi.filterexpression.Library;
-import org.zalando.nakadi.service.publishing.NakadiAuditLogPublisher;
+import org.zalando.nakadi.exceptions.runtime.AccessDeniedException;
+import org.zalando.nakadi.exceptions.runtime.InconsistentStateException;
+import org.zalando.nakadi.exceptions.runtime.RepositoryProblemException;
+import org.zalando.nakadi.exceptions.runtime.TimelineException;
+import org.zalando.nakadi.exceptions.runtime.TopicRepositoryException;
+import org.zalando.nakadi.filterexpression.FilterExpressionCompiler;
 import org.zalando.nakadi.view.EvalFilterRequest;
 import org.zalando.nakadi.view.EvalFilterResponse;
-import org.zalando.nakadi.view.TimelineRequest;
 import org.zalando.nakadisqlexecutor.streams.EventsWrapper;
 
 import java.util.Map;
-import java.util.Optional;
 import java.util.function.Function;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
@@ -32,7 +35,7 @@ public class DebugController {
             RepositoryProblemException {
         try {
             final byte[] eventBytes = evalFilterRequest.getEvent().toString().getBytes();
-            final Library library = new Library();
+            final FilterExpressionCompiler library = new FilterExpressionCompiler();
             final EventsWrapper predicateInput = library.singletonInput(eventBytes);
 
             Criterion criterion;
@@ -41,7 +44,8 @@ public class DebugController {
             try {
                 criterion = library.parseExpression(evalFilterRequest.getFilter());
             } catch (final SqlParserException e) {
-                return reportError(HttpStatus.BAD_REQUEST, "SQL_PARSER_ERROR", "filter expression could not be parsed", e);
+                return reportError(HttpStatus.BAD_REQUEST,
+                        "SQL_PARSER_ERROR", "filter expression could not be parsed", e);
             } catch (final Exception e) {
                 return reportError(HttpStatus.INTERNAL_SERVER_ERROR, "SQL_PARSER_UNEXPECTED_ERROR",
                         "an unexpected error occurred while parsing the filter expression", e);
@@ -69,7 +73,8 @@ public class DebugController {
         }
     }
 
-    private ResponseEntity<?> reportError(final HttpStatus httpStatus, final String error, final String message, final Exception e) {
+    private ResponseEntity<?> reportError(
+            final HttpStatus httpStatus, final String error, final String message, final Exception e) {
         // TODO use Problem class for error responses
         return ResponseEntity.status(httpStatus).body(Map.of(
                 "error", error,
